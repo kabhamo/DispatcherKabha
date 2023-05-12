2ffaf248dc561ba1aec187ecdb7a22bff14c9203
kabhamo <mkabha54@gmail.com>
2023-05-13 01:58:06 +0300

    add: splash screen logic

--- src/routes/AuthStack.tsx
+++ src/routes/AuthStack.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 import { colors } from '../util/colors';
 import { AuthParamList } from './paramsList/AuthParamList';
+import SplashScreen from 'react-native-splash-screen'
 
 const Stack = createNativeStackNavigator<AuthParamList>();
 
 type AuthStackProps = {}
 
 export const AuthStack: React.FC<AuthStackProps> = ({ }) => {
+
+    useEffect(() => {
+        SplashScreen.hide();
+    })
     return (
         <NavigationContainer>
             <Stack.Navigator

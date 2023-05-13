7db0fb66c99805d7d5704d50cce765c8209066d2
kabhamo <mkabha54@gmail.com>
2023-05-13 03:06:46 +0300

    Add: splash logics for android without images

--- android/app/src/main/java/com/dispatcherkabha/MainActivity.java
+++ android/app/src/main/java/com/dispatcherkabha/MainActivity.java
@@ -2,6 +2,7 @@ package com.dispatcherkabha;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
@@ -40,6 +41,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
     super.onCreate(null);
   }
 }

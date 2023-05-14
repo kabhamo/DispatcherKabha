4c479d31df64430079f33c3e7b5822f8797c2edf
kabhamo <mkabha54@gmail.com>
2023-05-14 12:34:43 +0300

    add: React native material and drawer logic code

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,33 @@
 import { View, StyleSheet, Text } from 'react-native'
 import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { TapNavProps } from '../routes/paramsList/AppParamList';
+import { RouteProp } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-type HomeScreenProps = {
+const Drawer = createDrawerNavigator();
 
+type HomeScreenProps = {}
+
+const TestDrawer = ({ }) => {
+  return (
+    <View><Text>TestDrawer1</Text></View>
+  )
 }
-export const HomeScreen: React.FC<HomeScreenProps> = ({ }) => {
+
+const HomeDrawer = () => {
+  return (
+    <Drawer.Navigator >
+      <Drawer.Screen name="Feed" component={TestDrawer} />
+    </Drawer.Navigator>
+  )
+}
+
+export const HomeScreen: React.FC<TapNavProps<'Home'>> = ({ navigation }) => {
   return (
     <View style={styles.mainContainer}>
       <Text>HomeScreen</Text>
+      {/*<HomeDrawer />*/}
     </View>
   );
 }
@@ -15,7 +35,7 @@ export const HomeScreen: React.FC<HomeScreenProps> = ({ }) => {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    //justifyContent: 'center',
+    //alignItems: 'center',
   },
 })

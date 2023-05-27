7c47ec9a711687b43f636f88fb235cb20576f12f
kabhamo <mkabha54@gmail.com>
2023-05-27 14:00:43 +0300

    Add: image bug fix

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -14,19 +14,19 @@ export const PAGES: PageInterface[] = [
     title: 'Dispatcher',
     description:
       'Welcon to Dispatcher, the right way to read your  news. Just open the app',
-    source: require('../assets/svg/onBoardingOne.svg'),
+    source: require('../assets/svg/onBoardingScreen.svg'),
   },
   {
     title: 'Dispatcher',
     description:
       "Search your fields of intrest and the best part..",
-    source: require('../assets/svg/onBoardingOne.svg'),
+    source: require('../assets/svg/onBoardingScreen.svg'),
   },
   {
     title: 'Dispatcher',
     description:
       'Save all your articles for later, filter, learn and explore the lates news',
-    source: require('../assets/svg/onBoardingOne.svg'),
+    source: require('../assets/svg/onBoardingScreen.svg'),
   },
 ];
 
